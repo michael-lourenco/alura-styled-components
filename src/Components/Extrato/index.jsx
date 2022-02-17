ee17e370b44deb968322b28ca6f9d60078b6a981
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-17 20:29:02 -0300

    Itens

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Botao, Box } from '../../Components/UI';
 import { extratoLista } from '../../info';
+import Items from '../Items';
 
  const Extrato = () => {
     return (
         <Box>
-            <h2>Extrato</h2>
             {
                 extratoLista.updates.map(({id, type, from, value, date}) => {
                     return (
-                        <div key={id}>
-                            <div>{type}</div>
-                            <div>{from}</div>
-                        </div>
+                        <Items key={id} type = {type} from = {from} value = {value} date = {date}/>
                     )
                 })
             }
